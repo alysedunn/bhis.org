604d1fff772d66f7a45ffba77c7a11d4936c2846
Alyse Dunn <alyse.dunn@gmail.com>
2017-08-27 15:44:59 -0400

    Add aboutus.html, contact.html, questions.html.

--- Main/application.py
+++ Main/application.py
@@ -6,17 +6,25 @@ application = Flask(__name__)
 def homepage ():
     return render_template('index.html')
 
-@application.route('/pricing', methods=['GET', 'POST'])
-def pricing ():
-    return render_template('pricing.html')
+@application.route('/aboutus', methods=['GET', 'POST'])
+def aboutus ():
+    return render_template('aboutus.html')
+
+@application.route('/questions', methods=['GET', 'POST'])
+def questions ():
+    return render_template('questions.html')
 
 @application.route('/forms', methods=['GET', 'POST'])
 def forms ():
     return render_template('forms.html')
 
-@application.route('/test', methods=['GET', 'POST'])
-def test ():
-    return render_template('test.html')
+@application.route('/pricing', methods=['GET', 'POST'])
+def pricing ():
+    return render_template('pricing.html')
+
+@application.route('/contact', methods=['GET', 'POST'])
+def contact ():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     application.debug = False
