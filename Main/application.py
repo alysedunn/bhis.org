029b4cd2576447596968bf2794204e0f6eb13a0f
Alyse Dunn <alyse.dunn@gmail.com>
2018-03-31 18:34:19 -0400

    Update reqs; clean up methods.

--- Main/application.py
+++ Main/application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mail import Mail, Message
 
 application = Flask(__name__)
@@ -15,7 +15,7 @@ application.config['MAIL_USE_SSL'] = True
 mail = Mail(application)
 
 msg = Message(
-    'Hello', sender='bhistest@gmail.com', recipients=['alyse.dunn@gmail.com'])
+    'Hello', sender='BHIS', recipients=[application.config['MAIL_USERNAME']])
 msg.body = "This is the email body"
 
 
@@ -24,30 +24,31 @@ def homepage():
     return render_template('index.html')
 
 
-@application.route('/aboutus', methods=['GET', 'POST'])
+@application.route('/aboutus', methods=['GET'])
 def aboutus():
     return render_template('aboutus.html')
 
 
-@application.route('/questions', methods=['GET', 'POST'])
+@application.route('/questions', methods=['GET'])
 def questions():
     return render_template('questions.html')
 
 
-@application.route('/forms', methods=['GET', 'POST'])
+@application.route('/forms', methods=['GET'])
 def forms():
     return render_template('forms.html')
 
 
-@application.route('/pricing', methods=['GET', 'POST'])
+@application.route('/pricing', methods=['GET'])
 def pricing():
     return render_template('pricing.html')
 
 
 @application.route('/contact', methods=['GET', 'POST'])
 def contact():
+    # name = request.form['username']
     mail.send(msg)
-    print str(msg)
+    # print str(name)
     return render_template('contact.html')
 
 
